2671d472c9a0d371d456f83ed1cf78398b392303
apw601 <apw601@gmail.com>
2018-06-15 17:09:57 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,7 @@
-class Model
-  # Replace with Dog clas
+def get_fortune()
+  #METHOD GOES HERE
+  fortunes_array = ["You'll have a great day", "Someone's going to go to the bathroom in 20 minutes", "You'll have sandwiches for the rest of your life", "You'll cry yourself to sleep tomorrow", "Delaney isn't as dope as Alex", "You'll become a grape", "Your hydroflask will explode", "You will be killed in Mafia"]
+  return fortunes_array.sample
 end
+
+puts get_fortune
